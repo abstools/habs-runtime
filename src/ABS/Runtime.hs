8333226c6ec34519c4c39d7638d4ac7264196388
Nikolaos Bezirgiannis <bezirg@gmail.com>
2016-06-02 16:43:20 +0200

    Export the Extension.Exception module

--- src/ABS/Runtime.hs
+++ src/ABS/Runtime.hs
@@ -1,9 +1,11 @@
 module ABS.Runtime
     ( module ABS.Runtime.Prim
     , module ABS.Runtime.Base
+    , module ABS.Runtime.Extension.Exception
     , module ABS.Runtime.Extension.Promise
     ) where
 
 import ABS.Runtime.Base (ABS', Sub' (..), Obj' (..), Fut, IORef', Null')
 import ABS.Runtime.Prim
+import ABS.Runtime.Extension.Exception
 import ABS.Runtime.Extension.Promise
