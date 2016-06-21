74a3cfe7f253def0f66d5abe4f149a4d58cf96b5
Nikolaos Bezirgiannis <bezirg@gmail.com>
2016-06-21 16:17:21 +0200

    Redirect failed exception messages to stderr instead

--- src/ABS/Runtime/Prim.hs
+++ src/ABS/Runtime/Prim.hs
@@ -31,6 +31,7 @@ import Control.Monad ((<$!>), when, unless, join)
 import Control.Exception (AssertionFailed, SomeException, Exception, throw, evaluate)
 import Control.Monad.Catch (catch, catches, Handler (..))
 import Data.Time.Clock.POSIX (getPOSIXTime) -- for realtime
+import System.IO (hPutStrLn, hPrint,stderr)
 import qualified System.Exit (exitFailure)
 import Data.Ratio (Ratio)
 import Data.Typeable
@@ -209,9 +210,9 @@ sync' (Obj' _ (Cog _ thisCogToken)) callee@(Obj' _ (Cog _ calleeCogToken)) metho
   fut <- newEmptyMVar 
   atomically $ writeTQueue otherMailBox (do
                               res <- methodPartiallyApplied obj `catches` 
-                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ print ex >> System.Exit.exitFailure
+                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ hPrint stderr ex >> System.Exit.exitFailure
                                       , Handler $ \ (ex :: SomeException) -> do
-                                          when (trace_exceptions cmdOpt) (lift $ putStrLn $ "Process died upon Uncaught-Exception: " ++ show ex)
+                                          when (trace_exceptions cmdOpt) (lift $ hPutStrLn stderr $ "Process died upon Uncaught-Exception: " ++ show ex)
                                           return $ throw ex  -- rethrows it inside the future-"box"                                       
                                       ]
                               lift $ putMVar fut res      -- method resolves future
@@ -226,9 +227,9 @@ sync' (Obj' _ (Cog _ thisCogToken)) callee@(Obj' _ (Cog _ calleeCogToken)) metho
   atomically $ writeTQueue otherMailBox (do
                -- we throw away the result (if we had "destiny" primitive then this optimization could not be easily applied
                (() <$ methodPartiallyApplied obj) `catches`
-                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ print ex >> System.Exit.exitFailure
+                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ hPrint stderr ex >> System.Exit.exitFailure
                                       , Handler $ \ (ex :: SomeException) ->
-                                          when (trace_exceptions cmdOpt) (lift $ putStrLn $ "Process died upon Uncaught-Exception: " ++ show ex)
+                                          when (trace_exceptions cmdOpt) (lift $ hPutStrLn stderr $ "Process died upon Uncaught-Exception: " ++ show ex)
                                           -- does not need to rethrow it (store it) inside the future, because it will not be read anyway
                                       ]
                back' otherCog)
@@ -244,9 +245,9 @@ awaitSugar' (Obj' _ thisCog@(Cog _ thisMailBox)) lhs obj@(Obj' _ otherCog@(Cog _
   callCC (\ k -> do
     lift $ atomically $ writeTQueue otherMailBox (do
                res <- methodPartiallyApplied obj `catches`
-                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ print ex >> System.Exit.exitFailure
+                                      [ Handler $ \ (ex :: AssertionFailed) -> lift $ hPrint stderr ex >> System.Exit.exitFailure
                                       , Handler $ \ (ex :: SomeException) -> do
-                                          when (trace_exceptions cmdOpt) (lift $ putStrLn $ "Process died upon Uncaught-Exception: " ++ show ex)
+                                          when (trace_exceptions cmdOpt) (lift $ hPutStrLn stderr $ "Process died upon Uncaught-Exception: " ++ show ex)
                                           return $ throw ex  -- rethrows it inside the future-"box"                                       
                                       ]
                lift $ atomically $ writeTQueue thisMailBox (do
@@ -346,8 +347,8 @@ main_is' mainABS' = runInUnboundThread $ do
   st <- newIORef []
   evalContT $ do
     (mainABS' $ Obj' (error "runtime error: the main ABS' block tried to call 'this'") (Cog st mb)) `catches`
-      [ Handler $ \ (ex :: AssertionFailed) -> lift $ print ex >> System.Exit.exitFailure
+      [ Handler $ \ (ex :: AssertionFailed) -> lift $ hPrint stderr ex >> System.Exit.exitFailure
         -- the main block does not have an associated future to store the exception
-      , Handler $ \ (ex :: SomeException) -> when (trace_exceptions cmdOpt) (lift $ putStrLn $ "Process died upon Uncaught-Exception: " ++ show ex)
+      , Handler $ \ (ex :: SomeException) -> when (trace_exceptions cmdOpt) (lift $ hPutStrLn stderr $ "Process died upon Uncaught-Exception: " ++ show ex)
       ] 
     when (keep_alive cmdOpt) $ back' (Cog st mb) -- if we want the main not to exit too early, we pass keep-alive that keeps the ABS' program alive forever
