02a55b192819ef930d1b9ad874cc56a774bde380
Nikolaos Bezirgiannis <bezirg@gmail.com>
2016-10-11 16:54:51 +0200

    WIP: rest cmdopt

--- src/ABS/Runtime/CmdOpt.hs
+++ src/ABS/Runtime/CmdOpt.hs
@@ -17,7 +17,7 @@ data CmdOpt = CmdOpt {
 cmdOptSpec :: CmdOpt
 cmdOptSpec = CmdOpt { 
     trace_exceptions = def &= help "The COGs hide any uncaught exceptions by default. This option logs to the stdout an uncaught exception. Used for debugging"
-  , http_port = 8080 &= help "The port that the REST HTTP server listens on. Defaults to 8080"
+  , http_port = 8080 &=name "p" &= typ "NUM" &= help "The port that the REST HTTP server listens on. Defaults to 8080"
              }
              &= program "habs-runtime" 
              &= help "The ABS-Haskell runtime" 
